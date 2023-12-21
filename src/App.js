07b0566895fdb7da62cc9c9b4f8ea15912e1afd9
David Cui <vcadavidcui@gmail.com>
2023-12-21 09:31:50 -0800

    inital

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {useState} from 'react'
+import Header from './Components/Header'
+import FeedbackList from './Components/FeedbackList'
+import FeedbackStats from './Components/FeedbackStats'
+import FeedbackForm from './Components/FeedbackForm'
+import AboutIconLink from './Components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
+import {FeedbackProvider} from './context/FeedbackContext'
+
+function App() {
+  return (
+    <FeedbackProvider>
+    <Router>
+      <Header />
+      <div className='container'>
+        <Routes>
+          <Route exact path='/' element={
+            <>
+              <FeedbackForm />
+              <FeedbackStats />
+              <FeedbackList />
+            </>
+          }> </Route>
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink />
+      </div>
+    </Router>
+    </FeedbackProvider>
+  )
+}
+
+export default App
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,20 @@
+import {Link} from 'react-router-dom'
+import Card from '../Components/shared/Card'
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About This Project</h1>
+        <p>This a React App to leave feedback for a product or service</p>
+        <p>Version: 1.0.0</p>
+
+        <p>
+          <Link to='/'>Back to Home</Link>
+        </p>
+      </div>
+    </Card>
+  )
+}
+
+export default AboutPage
